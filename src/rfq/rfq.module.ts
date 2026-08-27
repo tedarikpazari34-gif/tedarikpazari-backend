9dbf2ee9fe7bd2ca243fc366901df72722c13b77
NAZ ÇETİN <nazcetin@192.168.1.109>
2026-08-27 22:38:23 +0300

    Import MailModule in RFQ module

--- src/rfq/rfq.module.ts
+++ src/rfq/rfq.module.ts
@@ -3,9 +3,10 @@ import { RfqController } from './rfq.controller';
 import { RfqService } from './rfq.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationModule } from '../notification/notification.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [PrismaModule, NotificationModule],
+  imports: [PrismaModule, NotificationModule, MailModule],
   controllers: [RfqController],
   providers: [RfqService],
 })
